d4ac2b65d9d7316f3e58aaba360d9908e98982fe
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-23 16:54:14 +0100

    mail with method

--- javascript/main.js
+++ javascript/main.js
@@ -19,33 +19,40 @@ const app = new Vue ({
 
     for (let i = 0; i < 10; i++) {
 
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (response) {
-      // handle success
-      self.emailAddress = response.data.response;
-
-      self.mailList.push(self.emailAddress)
-
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-
+     
+      this.randomEmail()
 
     }
 
-
-   
-
-    
-
-
-
-   
-
-    
    },
+
+   methods: {
+
+
+     randomEmail() {
+
+    const self = this;
+
+
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(function (response) {
+        // handle success
+        self.emailAddress = response.data.response;
+  
+        self.mailList.push(self.emailAddress)
+  
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+
+     }
+      
+     
+   }
+
+   
 
 
  
