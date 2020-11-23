6ee66e1e60c93a337bc6068c68dcb15d22679500
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-23 16:37:05 +0100

    10 mails ok

--- javascript/main.js
+++ javascript/main.js
@@ -15,13 +15,16 @@ const app = new Vue ({
 
     const self = this;
 
-  
+    self.mailList.push()
+
+    for (let i = 0; i < 10; i++) {
 
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(function (response) {
       // handle success
       self.emailAddress = response.data.response;
 
+      self.mailList.push(self.emailAddress)
 
     })
     .catch(function (error) {
@@ -29,11 +32,19 @@ const app = new Vue ({
       console.log(error);
     });
 
-    
+
+    }
+
+
+   
 
     
 
-  
+
+
+   
+
+    
    },
 
 
